edbcb12493aeccca829ca9c127521abf32521f2c
kk <abcdxyzk@sina.com>
2014-11-20 01:00:26 +0800

    取消首字母自动大写

--- plugins/titlecase.rb
+++ plugins/titlecase.rb
@@ -21,12 +21,12 @@ class String
 
   def smart_capitalize
     # ignore any leading crazy characters and capitalize the first real character
-    if self =~ /^['"\(\[']*([a-z])/
-      i = index($1)
-      x = self[i,self.length]
+#    if self =~ /^['"\(\[']*([a-z])/
+#      i = index($1)
+#      x = self[i,self.length]
       # word with capitals and periods mid-word are left alone
-      self[i,1] = self[i,1].upcase unless x =~ /[A-Z]/ or x =~ /\.\w+/
-    end
+#      self[i,1] = self[i,1].upcase unless x =~ /[A-Z]/ or x =~ /\.\w+/
+#    end
     self
   end
 
