2edd8933e349e20477d1990b8b43d1df5234f3c7
kk <abcdxyzk@sina.com>
2014-11-23 04:05:24 +0800

    微调category的css

--- plugins/category_list_tag.rb
+++ plugins/category_list_tag.rb
@@ -18,7 +18,7 @@
 					if cats[0] > "0000" and cats[0] < "3000" # 如果是年，则单列
 						if cats.size == 2
 							if lt1 == 0
-								htmltime << "<div id='#{pret1}' class='divclassdate'>"
+								htmltime << "<div id='#{pret1}' class='catsub'>"
 								lt1 = 1
 							end
 							htmltime << "<li><a href='/#{category_dir}/#{category.to_url}/?opendiv=#{pret1}'>#{cats[0]}-#{cats[1]}</a>"
@@ -29,13 +29,13 @@
 								lt1 = 0
 							end
 							pret1 = cats[0]
-							htmltime << "<li class='categoryclass'><a href='/#{category_dir}/#{category.to_url}/'>#{category}</a><a href='##' onmousedown=showDiv('#{pret1}')><span class='exp_style' id='exp_#{pret1}'>[+]</span></a>"
+							htmltime << "<li class='catclass'><a href='/#{category_dir}/#{category.to_url}/'>#{category}</a><a href='##' onmousedown=showDiv('#{pret1}')><span class='exp_style'>[+]</span></a>"
 
 							htmltime << "<span class='right_span'>(#{posts_in_category})</span></li>\n"
 						end
 					elsif cats.size == 3
 						if l2 == 0
-							html << "<div id='#{pre1}~#{pre2}' class='divclasssub'>"
+							html << "<div id='#{pre1}~#{pre2}' class='catsub2'>"
 							l2 = 1
 						end
 						html << "<li><a href='/#{category_dir}/#{category.to_url}/?opendiv=#{pre1}~#{pre2}'>#{cats[2]}</a>"
@@ -46,15 +46,15 @@
 							html << "</div>"
 						end
 						if pre2 != ""
-							html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('aexp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n</script>\n"
+							html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('exp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n</script>\n"
 						end
 						if l1 == 0
-							html << "<div id='#{pre1}' class='divclass'>"
+							html << "<div id='#{pre1}' class='catsub'>"
 							l1 = 1
 						end
 						l2 = 0
 						pre2 = cats[1]
-						html << "<li><a href='/#{category_dir}/#{category.to_url}/?opendiv=#{pre1}'>#{cats[1]}</a><a href='##' onmousedown=showDiv('#{pre1}~#{pre2}') id='aexp_#{pre1}~#{pre2}'><span class='exp_style' id='exp_#{pre1}~#{pre2}'>[+]</span></a>"
+						html << "<li><a href='/#{category_dir}/#{category.to_url}/?opendiv=#{pre1}'>#{cats[1]}</a><a href='##' onmousedown=showDiv('#{pre1}~#{pre2}') id='exp_#{pre1}~#{pre2}'><span class='exp_style'>[+]</span></a>"
 						html << "<span class='right_span'>#{posts_in_category}</span></li>\n"
 					else
 						if l2 > 0
@@ -65,9 +65,9 @@
 						end
 						if pre1 != ""
 							# 如果一级、二级标签下面没有再分类则不展示'展开标签'
-							html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}')) document.getElementById('aexp_#{pre1}').style.visibility = 'hidden';\n"
+							html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}')) document.getElementById('exp_#{pre1}').style.visibility = 'hidden';\n"
 							if pre2 != ""
-								html << "if (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('aexp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n"
+								html << "if (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('exp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n"
 							end
 							html << "</script>\n"
 						end
@@ -75,7 +75,7 @@
 						l2 = 0
 						pre1 = cats[0]
 						pre2 = ""
-						html << "<li class='categoryclass'><a href='/#{category_dir}/#{category.to_url}/'>#{category}</a><a href='##' onmousedown=showDiv('#{pre1}') id='aexp_#{pre1}'><span class='exp_style' id='exp_#{pre1}'>[+]</span></a>"
+						html << "<li class='catclass'><a href='/#{category_dir}/#{category.to_url}/'>#{category}</a><a href='##' onmousedown=showDiv('#{pre1}') id='exp_#{pre1}'><span class='exp_style'>[+]</span></a>"
 
 						html << "<span class='right_span'>(#{posts_in_category})</span></li>\n"
 					end
@@ -88,9 +88,9 @@
 				end
 				if pre1 != ""
 					# 如果一级、二级标签下面没有再分类则不展示'展开标签'
-					html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}')) document.getElementById('aexp_#{pre1}').style.visibility = 'hidden';\n"
+					html << "<script language='javascript' type='text/javascript'>\nif (!document.getElementById('#{pre1}')) document.getElementById('exp_#{pre1}').style.visibility = 'hidden';\n"
 					if pre2 != ""
-						html << "if (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('aexp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n"
+						html << "if (!document.getElementById('#{pre1}~#{pre2}')) document.getElementById('exp_#{pre1}~#{pre2}').style.visibility = 'hidden';\n"
 					end
 					html << "</script>\n"
 				end
